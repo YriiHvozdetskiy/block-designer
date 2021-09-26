6493257aea6b43122b32343b336ba5df4bb5ba46
YriiHvozdetskiy <hvozdetskiyyrii@gmail.com>
2021-09-26 12:37:11 +0300

    add show img in form

--- src/js/block-designer.js
+++ src/js/block-designer.js
@@ -4,6 +4,7 @@ import '@pnotify/core/dist/Material.css';
 import '@pnotify/core/dist/PNotify.css';
 
 const refs = {
+  imageInput: document.querySelector('.image-input'),
   fromBtn: document.querySelector('.from-btn'),
   form: document.querySelector('.form-designer'),
   boxForBloks: document.querySelector('.box'),
@@ -12,16 +13,34 @@ const refs = {
   text: document.querySelector('.text'),
   boxList: document.querySelector('.box-list'),
   loader: document.querySelector('.loading-2--full-height'),
+  imgWrapper: document.querySelector('.image-wrapper'),
+  imageDisplay: document.querySelector('.image-display'),
 };
 
 const myError = error;
 
 refs.form.addEventListener('click', onForm);
+refs.imageInput.addEventListener('change', onImageInput);
 
 function isTextValid(value) {
   const cyrillicPattern = /^[\u0400-\u04FF]+$/;
   return cyrillicPattern.test(value);
 }
+
+let uploadedImage = '';
+
+function onImageInput() {
+  const reader = new FileReader();
+  reader.addEventListener('load', () => {
+    console.log('reader', reader);
+
+    uploadedImage = reader.result;
+    refs.imageDisplay.src = uploadedImage;
+  });
+  reader.readAsDataURL(this.files[0]);
+}
+
+let isValidForm = '';
 
 function formDataCollection() {
   const headingValue = refs.heading.value;
@@ -55,7 +74,7 @@ function formDataCollection() {
   <li class="box-item">
      <a href="${linkValue}" class="box-link">
         <div class="container-image">
-         <img src="" alt="">
+         <img src="${uploadedImage}" alt="">
         </div>
             <ul class="container-list">
                 <li class="container-item">${headingValue}</li>
@@ -66,6 +85,8 @@ function formDataCollection() {
 `;
 
   refs.boxList.insertAdjacentHTML('beforeend', el);
+
+  isValidForm = true;
 
   if (refs.boxList.children.length > 10 && refs.boxList.children.length < 12) {
     showLoadMoreBtn();
@@ -78,11 +99,19 @@ function onForm(e) {
   }
 
   refs.loader.classList.add('loading-2');
-  // if (!formDataCollection()) e.currentTarget.reset();
   formDataCollection();
+  if (isValidForm) clearForm();
   refs.loader.classList.remove('loading-2');
 }
 
 function showLoadMoreBtn() {
   refs.loadMore.classList.remove('is-hiden');
 }
+
+const clearForm = () => {
+  refs.heading.value = '';
+  refs.link.value = '';
+  refs.text.value = '';
+  // зробити нормально
+  refs.imageDisplay.src = '';
+};
